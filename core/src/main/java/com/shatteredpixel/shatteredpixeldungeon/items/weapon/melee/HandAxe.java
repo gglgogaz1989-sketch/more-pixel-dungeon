e615126cb26a188b254857760a7f60f1cf1c35aa
gglgog <gglgogaz1989@gmail.com>
2026-08-28 14:43:03 +0300

    Increase accuracy and damage scaling for HandAxe

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/HandAxe.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/HandAxe.java
@@ -34,12 +34,12 @@ public class HandAxe extends MeleeWeapon {
 		hitSoundPitch = 1f;
 
 		tier = 2;
-		ACC = 1.32f; //32% boost to accuracy
+		ACC = 1.45f; //45% boost to accuracy
 	}
 
 	@Override
 	public int max(int lvl) {
-		return  4*(tier+1) +    //12 base, down from 15
+		return  5*(tier+1) +    //12 base, down from 15
 				lvl*(tier+1);   //scaling unchanged
 	}
 
@@ -51,13 +51,13 @@ public class HandAxe extends MeleeWeapon {
 	@Override
 	protected void duelistAbility(Hero hero, Integer target) {
 		//+(4+1.5*lvl) damage, roughly +55% base dmg, +75% scaling
-		int dmgBoost = augment.damageFactor(4 + Math.round(1.5f*buffedLvl()));
+		int dmgBoost = augment.damageFactor(5 + Math.round(1.7f*buffedLvl()));
 		Mace.heavyBlowAbility(hero, target, 1, dmgBoost, this);
 	}
 
 	@Override
 	public String abilityInfo() {
-		int dmgBoost = levelKnown ? 4 + Math.round(1.5f*buffedLvl()) : 4;
+		int dmgBoost = levelKnown ? 5 + Math.round(1.7f*buffedLvl()) : 5;
 		if (levelKnown){
 			return Messages.get(this, "ability_desc", augment.damageFactor(min()+dmgBoost), augment.damageFactor(max()+dmgBoost));
 		} else {
@@ -66,7 +66,7 @@ public class HandAxe extends MeleeWeapon {
 	}
 
 	public String upgradeAbilityStat(int level){
-		int dmgBoost = 4 + Math.round(1.5f*level);
+		int dmgBoost = 5 + Math.round(1.7f*level);
 		return augment.damageFactor(min(level)+dmgBoost) + "-" + augment.damageFactor(max(level)+dmgBoost);
 	}
 
